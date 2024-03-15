eb0cb7f1d606a1a1e83975c15f04e694dabbeb39
Tomas Panelo <topanelo2016@gmail.com>
2024-03-15 20:36:52 -0300

    Modificacion para que en vez de intentos tenga fallos

--- game.py
+++ game.py
@@ -1,10 +1,11 @@
 import random
 # Lista de palabras posibles
-words = ["python", "programación", "computadora", "código", "desarrollo","inteligencia"]
+words = ["python", "programación", "computadora", "código", "desarrollo",
+"inteligencia"]
 # Elegir una palabra al azar
 secret_word = random.choice(words)
 # Número máximo de intentos permitidos
-max_attempts = 10
+failures = 0
 # Lista para almacenar las letras adivinadas
 guessed_letters = []
 print("¡Bienvenido al juego de adivinanzas!")
@@ -12,7 +13,7 @@ print("Estoy pensando en una palabra. ¿Puedes adivinar cuál es?")
 word_displayed = "_" * len(secret_word)
 # Mostrarla palabra parcialmente adivinada
 print(f"Palabra: {word_displayed}")
-for i in range(max_attempts):
+while failures < 10:
     # Pedir al jugador que ingrese una letra
     letter = input("Ingresa una letra: ").lower()
     # Verificar si la letra ya ha sido adivinada
@@ -29,6 +30,7 @@ for i in range(max_attempts):
         print("¡Bien hecho! La letra está en la palabra.")
     else:
         print("Lo siento, la letra no está en la palabra.")
+        failures += 1
     # Mostrar la palabra parcialmente adivinada
     letters = []
     for letter in secret_word:
@@ -43,5 +45,5 @@ for i in range(max_attempts):
         print(f"¡Felicidades! Has adivinado la palabra secreta: {secret_word}")
         break
 else:
-    print(f"¡Oh no! Has agotado tus {max_attempts} intentos.")
+    print(f"¡Oh no! Has llegado a tus {failures} fallos.")
     print(f"La palabra secreta era: {secret_word}")
